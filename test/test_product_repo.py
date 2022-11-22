443e020515cba377cfb7fd7ea92583cf794d468b
nayan gurung <nayangrg.uni@gmail.com>
2022-11-22 19:03:39 +0000

    Add test cases to test_product_repo

--- test/test_product_repo.py
+++ test/test_product_repo.py
@@ -3,49 +3,48 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from src.product.product import Product
-from product.product_repository import ProductRepo
-
+from src.product.product_repository import ProductFileRepository
 
 
 import pytest
 
 @pytest.fixture()
-def fake_product_repo():
+def product_repository():
 
-    fake_product_repo = ProductRepo("resources/fake_product.csv")
-    yield fake_product_repo
+    product_repository = ProductFileRepository(r"C:\Users\NayanGurung\Generation\Mini_Project\test\resources\fake_product.csv")
+    yield product_repository
     
     product1 = Product("Coke", 2.2)
     product2 = Product("Fanta", 2.0)
-    fake_product_repo.list = [product1, product2]
-    fake_product_repo.save()
+    product_repository.list = [product1, product2]
+    product_repository.save()
 
 
 
-def test_add(fake_product_repo): 
+def test_add(product_repository):
     p = Product("cheese", 2)
-    fake_product_repo.add(p)
-    assert fake_product_repo.list[-1].name == "Cheese"
+    product_repository.add(p)
+    assert product_repository.list[-1].name == "Cheese"
 
-def test_update(fake_product_repo):
+def test_update(product_repository):
     mock_updated_product_details = {"name": "Water"}
-    fake_product_repo.update(0, mock_updated_product_details)
-    assert fake_product_repo.list[0].name == "Water"
+    product_repository.update(0, mock_updated_product_details)
+    assert product_repository.list[0].name == "Water"
 
 
-def test_delete(fake_product_repo):
-    fake_product_repo.delete(0)
-    assert len(fake_product_repo.list) == 1
+def test_delete(product_repository):
+    product_repository.delete(0)
+    assert len(product_repository.list) == 1
 
-def test_get(fake_product_repo):
-    products = fake_product_repo.get()
+def test_get(product_repository):
+    products = product_repository.get()
     assert len(products) == 2
     pass
 
-def test_save(fake_product_repo):
-    fake_product_repo.add(Product("Chocolate", 3.3))
-    fake_product_repo.save()
-    assert len(fake_product_repo.list) == 3
+def test_save(product_repository):
+    product_repository.add(Product("Chocolate", 3.3))
+    product_repository.save()
+    assert len(product_repository.list) == 3
 
 
 
