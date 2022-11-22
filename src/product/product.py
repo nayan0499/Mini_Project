d9a22eb7aa062756f971971f6bf55f9af07c0683
nayan gurung <nayangrg.uni@gmail.com>
2022-11-22 14:59:05 +0000

    Apply style update to product.py

--- src/product/product.py
+++ src/product/product.py
@@ -1,87 +1,82 @@
-import sys 
+import sys
 from pathlib import Path
+
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 
 class Product:
-    
-    keys = ["name", "price"]
+
+    keys = ['name', 'price']
 
     def __init__(self, name: str, price: float):
         if name.isnumeric():
-            raise TypeError("Name must not be numeric")
+            raise TypeError('Name must not be numeric')
         elif price < 0.0:
-            raise TypeError("Price must be greater than 0")
+            raise TypeError('Price must be greater than 0')
         else:
             self.price = price
             self.name = name.title()
-        
+
     @classmethod
     def object_to_list(cls, object):
-        if not isinstance(object,Product): 
-            raise TypeError("Object needs be an instance of Product")
+        if not isinstance(object, Product):
+            raise TypeError('Object needs be an instance of Product')
         return [object.name, object.price]
-
 
     @classmethod
     def dict_to_obj(cls, dict):
         try:
-            product = Product(dict["name"],float(dict["price"]))
+            product = Product(dict['name'], float(dict['price']))
             return product
-        except KeyError: 
-            return -1 
-    
-            
-    
+        except KeyError:
+            return -1
 
     @classmethod
     def create_item_from_user_input(cls):
-        while True: 
+        while True:
             try:
                 new_product = Product(
-                    input("Product name: ").title(),
-                    float(input("Product price: "))
+                    input('Product name: ').title(),
+                    float(input('Product price: ')),
                 )
                 return new_product
             except ValueError:
-                print("price must be a float")
-                continue 
-                
+                print('price must be a float')
+                continue
+
             except TypeError as x:
                 print(x)
-                continue 
+                continue
 
     @classmethod
     def get_update_details(cls):
 
         product_details = {}
-        for key in cls.keys: 
-            input_by_user =  input(f"{key}: ")
-            if input_by_user!= "":
+        for key in cls.keys:
+            input_by_user = input(f'{key}: ')
+            if input_by_user != '':
                 product_details[key] = input_by_user
-        try: 
+        try:
             product_details = cls.get_validated_update_details(product_details)
             return product_details
-        except TypeError as e: 
-            print(e) #TODO: 
+        except TypeError as e:
+            print(e)   # TODO:
             return cls.get_update_details()
-            
-
 
     @classmethod
     def get_validated_update_details(cls, product_details):
-        for k,v in product_details.items():
-            if k =="name": 
+        for k, v in product_details.items():
+            if k == 'name':
                 if v.isnumeric():
-                    raise TypeError("Invalid name- try again")
-                else: 
+                    raise TypeError('Invalid name- try again')
+                else:
                     product_details[k] = v.title()
-            elif k == "price":
+            elif k == 'price':
                 try:
                     product_details[k] = float(v)
-                except: 
-                    raise TypeError("price must only contain numbers -try again")
-        
+                except:
+                    raise TypeError(
+                        'price must only contain numbers -try again'
+                    )
 
         return product_details
-
